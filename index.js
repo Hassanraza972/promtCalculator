9651a1c2357cf3c66653c8e5146521628cd0d862
Hassan Raza <hassanraza12004@gmail.com>
2024-03-14 10:55:45 +0500

    my message

---
+++ index.js
@@ -0,0 +1,15 @@
+let FirstName = prompt("Enter Your Name")
+alert("welcome" + FirstName)
+let num1 = prompt("first number")
+let num2 = prompt("second number")
+num1=Number(num1)
+num2=Number(num2)
+console.log("num1",num1);
+console.log("num2",num2);
+
+let Add=num1 + num2
+let Sub=num1 - num2
+alert("Adition:" + Add)
+alert("Subtraction:" + Sub)
+alert("multipilication:" + num1 * num2)
+alert("division" + num1 / num2)
